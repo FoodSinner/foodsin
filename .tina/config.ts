9260c287106013fad3776f9d972cb57fbafbd918
FoodSinner <i.am.a.food.sinner@gmail.com>
2023-03-09 23:20:31 -0500

    refactoring config.ts

--- .tina/config.ts
+++ .tina/config.ts
@@ -1,16 +1,16 @@
 import { defineConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
-const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "master";
+const branch = process.env.NEXT_PUBLIC_TINA_BRANCH || process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "master";
 // const branch =
 //   process.env.NEXT_PUBLIC_TINA_BRANCH ||
 //   process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF ||
 //   process.env.HEAD
 
 export default defineConfig({
+  token: 'cae1d6a882de8fca4571726e481176e0ed1d846b', // Get this from tina.io
+  clientId: '46172bd6-a7e4-4e4c-83f1-fd0761ff91ca', // Get this from tina.io
   branch,
-  clientId: "46172bd6-a7e4-4e4c-83f1-fd0761ff91ca", // Get this from tina.io
-  token: "cae1d6a882de8fca4571726e481176e0ed1d846b", // Get this from tina.io
   build: {
     outputFolder: "admin",
     publicFolder: "static",
