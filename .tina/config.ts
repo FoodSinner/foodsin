f18cd62983b180065aba35b0a2c7d0306cc4d754
FoodSinner <i.am.a.food.sinner@gmail.com>
2023-03-10 00:21:57 -0500

    built results

--- .tina/config.ts
+++ .tina/config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
 // const branch = process.env.NEXT_PUBLIC_TINA_BRANCH || process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "master";
-const branch = "master";
-// const branch =
-//   process.env.NEXT_PUBLIC_TINA_BRANCH ||
-//   process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF ||
-//   process.env.HEAD
+// const branch = "master";
+const branch =
+  process.env.NEXT_PUBLIC_TINA_BRANCH ||
+  process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF ||
+  process.env.HEAD ||
+  "master";
 
 export default defineConfig({
   token: 'cae1d6a882de8fca4571726e481176e0ed1d846b', // Get this from tina.io
@@ -58,7 +59,7 @@ export default defineConfig({
             list: true,
           },
           {
-            type: "image", 
+            type: "string", 
             name: "project_images", 
             label: "Project Images", 
             list: true,
